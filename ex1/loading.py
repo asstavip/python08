723a755e7d7adf531c6c9d5c82f53fcaa30382d3
asstavip <abdo.bissari@gmail.com>
2026-02-06 07:19:04 +0100

    done

--- ex1/loading.py
+++ ex1/loading.py
@@ -2,24 +2,36 @@ print("LOADING STATUS: Loading programs...")
 try:
     import pandas as pd
     import requests as req
-    import matplotlib
+    import matplotlib.pyplot as plt
 except ImportError as e:
     print(e)
     module = str(e).split("'")[-2]
-    print("install module using: pip install",module)
-    print(
-        "or you can use to install all dependencies:" \
-        " pip install -r requirement.txt"
-          )
+    print("install module using: pip install", module)
+    print("or you can use to install all dependencies: pip install -r requirement.txt")
 else:
-    packages = [pd,req,matplotlib]
+    packages = [pd, req]
     for pack in packages:
         print(f"[OK] {pack.__name__} {pack.__version__} - Data manipulation ready")
 
-data = {
-    "time": range(1,11),
-    "value": range(101,111)
-}
+data = {"time": range(1, 11), "value": range(101, 111)}
 df = pd.DataFrame(data)
+print(df.head(6))
 
-print(df)
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 3, 6, 5]
+
+
+plt.plot(x, y)
+plt.title("Sample Plot")
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.savefig("sample_plot.png")
+print("Plot saved as sample_plot.png")
+
+response = req.get("https://jsonplaceholder.typicode.com/todos/1")
+if response.status_code == 200:
+    print("HTTP Request Successful")
+    print("Response JSON:", response.json())
+    
+
+print("All programs loaded successfully.")
